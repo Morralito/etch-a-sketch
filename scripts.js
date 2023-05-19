9f939cd641dbe6aee2c7fd43cc853ead98c60982
morralito <3699650+Morralito@users.noreply.github.com>
2023-05-19 16:56:39 -0400

    add childnode delete, slider grid size selector

--- scripts.js
+++ scripts.js
@@ -1,12 +1,35 @@
 const container = document.querySelector('.gridContainer');
-let squareSize = parseInt(prompt('Enter canvas size: '));
+const sliderValue = document.getElementById('gridNumber');
+const valueOuput = document.getElementById('currentValue');
 
-for (let i = 0; i < squareSize**2; i++){
-    let gridSquare = document.createElement('div');
-    gridSquare.classList.add('gridSquare');
-    gridSquare.style.width = `${480/squareSize}px`;
-    gridSquare.style.width = `${480/squareSize}px`;
-    gridSquare.addEventListener('mouseover', (event) => event.target.style.backgroundColor = 'black');
-    container.appendChild(gridSquare);
+
+createGrid(16);
+
+sliderValue.addEventListener('input', function(event) {
+    const squareSize = parseInt(event.target.value);
+    valueOuput.textContent = squareSize + ' X ' + squareSize;
+    removeChilds(container);
+    createGrid(squareSize);
+});
+
+function createGrid(squareSize){
+
+    for (let i = 0; i < squareSize**2; i++){
+        let gridSquare = document.createElement('div');
+        gridSquare.classList.add('gridSquare');
+        gridSquare.style.width = `${650/squareSize}px`;
+        gridSquare.style.width = `${650/squareSize}px`;
+
+        gridSquare.addEventListener('mouseover', (event) => {
+            event.target.style.backgroundColor = 'black';
+            event.target.style.outline = '1px solid black';
+        });
+        container.appendChild(gridSquare);
+    }
 }
+
+function removeChilds(node) {
+    let last;
+    while (last = node.lastChild) node.removeChild(last);
+};
 
